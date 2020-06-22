068d7d447d311f8d2eaf0361ec6811a9a30296be
RajatSablok <rajat.main06@gmail.com>
2020-06-22 13:03:17 +0530

    errors handled

--- controllers/appControllers.js
+++ controllers/appControllers.js
@@ -33,19 +33,7 @@ const fileFilter = (req, file, cb) => {
   }
 };
 
-// const upload = multer({
-//   storage: storage,
-//   limits: {
-//     fileSize: 1024 * 1024 * 5,
-//   },
-//   fileFilter: fileFilter,
-// });
-
-// router.post("/upload", upload.single("file"), (req, res, next) => {
-//   console.log(req.file);
-//   res.status(200).json(req.file);
-// });
-
+//Upload timetable
 router.post("/upload", (req, res, next) => {
   var upload = multer({
     storage: storage,
@@ -65,26 +53,74 @@ router.post("/upload", (req, res, next) => {
     let filepath =
       path.dirname(__dirname) + "/public/uploads/" + req.file.filename;
 
-    // console.log(filepath);
     var options = {
       mode: "text",
-      args: [filepath], // pass arguments to the script here
+      args: [filepath],
     };
 
     PythonShell.run("./freeSlots.py", options, function (err, results) {
-      if (err) res.json({ err });
-      console.log(results);
+      if (err)
+        res.status(400).json({
+          message: "something went wrong",
+          error: err,
+        });
+      else {
+        var timetableArray = JSON.parse(results);
 
-      // new UserSlots({ name: req.body.text, timetable: results })
-      //   .save()
-      //   .then((data) => {
-      //     console.log(data);
-      //     // res.render("upload", { results: data });
-      //   })
-      //   .catch((err) => res.json({ err }));
-      // });
+        new UserSlots({
+          _id: new mongoose.Types.ObjectId(),
+          name: req.body.name,
+          semester: req.body.semester,
+          timetable: timetableArray,
+        })
+          .save()
+          .then((data) => {
+            res.status(201).json({
+              message: "Uploaded successfully",
+              result: data,
+            });
+          })
+          .catch((err) =>
+            res.status(400).json({
+              message: "Something went wrong",
+              error: err,
+            })
+          );
+      }
     });
   });
+});
+
+//Get names of all users
+router.get("/all", function (req, res) {
+  UserSlots.find({}, { _id: 0, name: 1 })
+    .then((data) => {
+      console.log(data);
+      res.status(200).json({
+        count: data.length,
+        result: data,
+      });
+    })
+    .catch((err) => console.log(err));
+});
+
+//Get timetable of one user
+router.get("/compare", (req, res, next) => {
+  var arr = req.query.check;
+  console.log(arr);
+  UserSlots.find(
+    { name: { $in: arr } },
+    { _id: 0, name: 1, semester: 1, timetable: 1 }
+  )
+    .then((data) => {
+      res.status(200).json({
+        message: "success",
+        result: data,
+      });
+    })
+    .catch((err) => {
+      res.status(400).json({ error: err });
+    });
 });
 
 module.exports = router;
