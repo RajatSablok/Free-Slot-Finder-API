b32259d556e71338993f00410fea52ddbb974733
RajatSablok <rajat.main06@gmail.com>
2020-06-22 12:16:58 +0530

    timetable check

--- controllers/appControllers.js
+++ controllers/appControllers.js
@@ -8,7 +8,7 @@ const multer = require("multer");
 
 const router = express.Router();
 
-const userSchema = require("../models/userSlot");
+const UserSlots = require("../models/userSlot");
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
@@ -33,17 +33,58 @@ const fileFilter = (req, file, cb) => {
   }
 };
 
-const upload = multer({
-  storage: storage,
-  limits: {
-    fileSize: 1024 * 1024 * 5,
-  },
-  fileFilter: fileFilter,
-});
+// const upload = multer({
+//   storage: storage,
+//   limits: {
+//     fileSize: 1024 * 1024 * 5,
+//   },
+//   fileFilter: fileFilter,
+// });
 
-router.post("/upload", upload.single("file"), (req, res, next) => {
-  console.log(req.file);
-  res.status(200).json(req.file);
+// router.post("/upload", upload.single("file"), (req, res, next) => {
+//   console.log(req.file);
+//   res.status(200).json(req.file);
+// });
+
+router.post("/upload", (req, res, next) => {
+  var upload = multer({
+    storage: storage,
+    limits: {
+      fileSize: 1024 * 1024 * 5,
+    },
+    fileFilter: fileFilter,
+  }).single("file");
+
+  upload(req, res, (err) => {
+    if (err) {
+      return res.status(400).json({
+        error: err,
+      });
+    }
+
+    let filepath =
+      path.dirname(__dirname) + "/public/uploads/" + req.file.filename;
+
+    // console.log(filepath);
+    var options = {
+      mode: "text",
+      args: [filepath], // pass arguments to the script here
+    };
+
+    PythonShell.run("./freeSlots.py", options, function (err, results) {
+      if (err) res.json({ err });
+      console.log(results);
+
+      // new UserSlots({ name: req.body.text, timetable: results })
+      //   .save()
+      //   .then((data) => {
+      //     console.log(data);
+      //     // res.render("upload", { results: data });
+      //   })
+      //   .catch((err) => res.json({ err }));
+      // });
+    });
+  });
 });
 
 module.exports = router;
